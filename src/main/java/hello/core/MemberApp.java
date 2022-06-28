7945f974174554f2d413c5833715feb7d7780973
muttjimmypark <muttpark57@gmail.com>
2022-06-28 19:30:56 +0900

    주문, 할인 도메인 설계와 개발

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -6,7 +6,9 @@ import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 
 public class MemberApp {
+
     public static void main(String[] args) {
+
         MemberService memberService = new MemberServiceImpl();
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
