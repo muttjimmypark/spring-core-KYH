c13c876143b5476f4d4689f2dfae8f8b98310940
muttjimmypark <muttpark57@gmail.com>
2022-06-29 17:57:35 +0900

    스프링 애노테이션 작성 + 컨테이너 생성

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,33 +9,33 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
     //서비스 구현체 선택 부분
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    //도메인(정책) 구현체 선택 부분
-    //db 전환시 테이블,원소가 되는 부분이 도메인 아닌가?
-    //그러면 이 부분은 컨트롤러-서비스-도메인-리포지토리 구분에서 서비스영역일까?
-    //서비스가 참조하니까 도메인영역? <-> 리포지토리는 참조하지 않으니까 서비스영역은 아닌듯
-    /*
-    나름 정리해보자면 컨트롤러 - 서비스 - 리포지토리
-                        - 도메인 - 정책
-     */
-    private DiscountPolicy discountPolicy() {
+    //정책 구현체 선택 부분
+    @Bean
+    public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
 
     //리포지토리 구현체 선택 부분
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 }
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -3,6 +3,8 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 //import hello.core.member.MemberServiceImpl;
 
 public class MemberApp {
@@ -10,8 +12,20 @@ public class MemberApp {
     public static void main(String[] args) {
 
 //        MemberService memberService = new MemberServiceImpl();
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        //스프링 컨테이너에 올라간 모든 객체들을 관리하는 AC라는걸 호출
+        // ㄴ> XML로 작성할수도 있고, 이거처럼 애노테이션 기반으로 작성할 수 있음
+        // ㄴ> AC 상위의 BeanFactory도 있지만 거의 사용x
+        // ㄴ> 따라서, ApplicationContext를 스프링 컨테이너라고 통용적으로 말한다
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        //Bean으로 컨테이너에 올려둔걸 (이름으로 get, 반환Type)
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        //이로써 코드의 제어권을 작성자가 갖는 : 라이브러리가 아닌
+        //프로그램 자신이 제어권을 갖는 : 프레임워크로 전환이 이루어진다
+        //(AppConfig를 직접 작성하는게 아니라, 프레임워크에 맡긴다고 작성한것)
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
