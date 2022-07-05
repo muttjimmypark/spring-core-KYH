7ee79305210f6ee91f63bb284aec36ea8a385478
muttjimmypark <muttpark57@gmail.com>
2022-07-05 13:28:25 +0900

    xml로 config + BeanDefinition 열람

---
+++ src/test/java/hello/core/xml/XmlAppContext.java
@@ -0,0 +1,19 @@
+package hello.core.xml;
+
+import hello.core.member.MemberService;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class XmlAppContext {
+
+    @Test
+    void xmlAppContext() {
+        //xml 문서를 보면, 구성은 동일하고 문법만 다르다는걸 알수 있
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
