7ebca068d6967e2040f726bc48875806ef167fb7
muttjimmypark <muttpark57@gmail.com>
2022-07-14 19:53:21 +0900

    의존관계 탐색을 적용하는 방법 + 웹 스코프 1. 리퀘스트

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -1,12 +1,15 @@
 package hello.core.scope;
 
-import org.assertj.core.api.Assertions;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -35,19 +38,69 @@ public class SingletonWithPrototypeTest1 {
         }
     }
 
+    /*
+    이렇게 해서는 싱글톤 생명주기에 프로토타입 스코프가 박혀있기 때문에
+    의도대로 프로토타입 스코프를 중복되지않게 생성할 수 없다
+     */
+//    @Scope("singleton")
+//    static class ClientBean {
+//        private final PrototypeBean prototypeBean;
+//
+//        public ClientBean(PrototypeBean prototypeBean) {
+//            this.prototypeBean = prototypeBean;
+//        }
+//
+//        public int logic() {
+//            prototypeBean.addCount();
+//            return prototypeBean.getCount();
+//        }
+//    }
+    /*
+    방법 1. ObjectProvider<T>를 사용
+    다양한 부가기능 메서드를 제공한다는 장점
+     */
+//    @Scope("singleton")
+//    static class ClientBean {
+//        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+//
+//        public ClientBean(ObjectProvider<PrototypeBean> prototypeBeanProvider) {
+//            this.prototypeBeanProvider = prototypeBeanProvider;
+//        }
+//        //다 되는데 RequiredArgsConstructor는 왜 적용이 안될까?
+//        //별도의 애노테이션이 있나...
+//
+//        public int logic() {
+//            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+//            prototypeBean.addCount();
+//            return prototypeBean.getCount();
+//        }
+//    }
+    /*
+    방법 2. 자바표준(JSR-330) Provider<T>를 사용
+    자바 표준이라 기능이 간결하고, 다른 컨테이너와도 호환되는게 장점
+     */
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean;
+        private Provider<PrototypeBean> prototypeBeanProvider;
 
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
+        public ClientBean(Provider<PrototypeBean> prototypeBeanProvider) {
+            this.prototypeBeanProvider = prototypeBeanProvider;
         }
 
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
     }
+    /*
+    결국 이 두가지의 방법은
+    좁게봐서 프로토타입 스코프로 정의한 빈이 의도대로 활용되게끔만을 위한 것이 아니라
+    넓게봐서 여러모로 DL(의존성 탐색)이 필요할때 쓰는 방법이라고 알면 되겠다
+    (기존 di 관점에서 봤을때
+    늦게 주입해야한다던지, 경우에따라 다른걸 주입받는다던지, 순환의존성이 필요하다던지)
+    거의 안쓰지만, 코드 분석시에 가끔 발견됨...?
+     */
 
     @Test
     void prototypeFind() {
@@ -72,6 +125,6 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 }
